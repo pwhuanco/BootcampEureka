edb190f449164b9764cb75f2a453f9cc07a20d89
pwhuanco <pedrohuanco@gmail.com>
2022-03-18 15:49:13 -0500

    comentarios

--- BootcampEurekaServer/src/main/java/com/bootcamp/eurekaserver/EurekaServerApplication.java
+++ BootcampEurekaServer/src/main/java/com/bootcamp/eurekaserver/EurekaServerApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * EurekaServerApplication.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
-    public static void main(String[] args) {
+    /**
+     * EurekaServerApplication.
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
